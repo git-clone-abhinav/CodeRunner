b0654c140ae5ef09bfa00c2b21d88e138284a73c
RaspberryPi <abhi.anu003@gmail.com>
2021-08-25 00:02:23 +0530

    logging

---
+++ logger.py
@@ -0,0 +1,11 @@
+import time
+
+def log_it(id,message):
+    
+    with open('/home/pi/Desktop/mailLogs.txt', 'a') as f:
+        f.write(f"{time.ctime()} - {id} {message}\n".format())
+        f.close()
+        
+# log_it(23123,"one")
+# log_it(23123,"two")
+# log_it(23123,"three")
